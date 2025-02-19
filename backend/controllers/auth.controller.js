e559d2b7dafeb453952200646af3c9ff287a88bd
rahulkumarhavit@gmail.com <allblogwork24@gmail.com>
2025-02-19 23:40:35 +0530

    token jwt

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -28,7 +28,7 @@ export const signup = async (req,res)=>{
         })
         await user.save();
         // jwt
-        generateTokenAndSetCookie(res,user_id);
+        generateTokenAndSetCookie(res,user._id);
     } catch (error) {   
         res.status(400).json({success: false, message: error.message})
     }
